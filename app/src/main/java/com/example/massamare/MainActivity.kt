4c00aa329bf47988287d94723b51257e9c3e7340
Tupiet <Tupiet@users.noreply.github.com>
2020-08-18 18:42:06 +0200

    3.0 - Added an Auto-Update mode (and a new cool website)
    The version 3.0 is finally here!
    This is, probably, the most important update. In this update, we added Firebase to the app, and a new website.
    
    # UPDATES:
    -Firebase support. This app now support the Firebase engine. So, with a few of adjustements, we can add those incredible features that Firebase has.
    -CHECK UPDATES: Yes! Using Firebase and the Remote Config, now, every time the app opens (and is connected to the internet), it would check if there is any update. If yes, it will say an Alert Message with a button, that will send the user to our official website.
    -Official website: We're releasing our new official website (https://massamare.tk). In this website, you can update (or install, if you haven't it) your app to the last version released.
    
    # COMING:
    -Probably, I'll add a notification system. This will allow me to send a notification when there is a new version.
    -Adding a new beta mode: this will allow me to test the app without having the new update alert every time.
    
    This is a special update that has been so hard for me, but finally is out!

--- app/src/main/java/com/example/massamare/MainActivity.kt
+++ app/src/main/java/com/example/massamare/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.massamare
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.Gravity
 import android.view.View
@@ -10,7 +11,11 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -21,7 +26,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button: Button = findViewById(R.id.button)
+        // region REMOTE CONFIG
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 5
+        }
+
+        val firebaseConfig = Firebase.remoteConfig
+        firebaseConfig.setConfigSettingsAsync((configSettings))
+        firebaseConfig.setDefaultsAsync(mapOf("min_version" to BuildConfig.VERSION_NAME))
+        firebaseConfig.setDefaultsAsync(mapOf("update_title" to "¡Nueva actualización!"))
+        firebaseConfig.setDefaultsAsync(mapOf("update_description" to "Deberías actualizar ya, ¡tiene nuevas opciones!"))
+
+        Firebase.remoteConfig.fetchAndActivate().addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val minVersion = Firebase.remoteConfig.getString("min_version")
+                val updateTitle = Firebase.remoteConfig.getString("update_title")
+                val updateDescription = Firebase.remoteConfig.getString("update_description")
+
+                val versionFinal = BuildConfig.VERSION_NAME
+
+                if (versionFinal != minVersion){
+                    val builder = AlertDialog.Builder(this)
+                    builder.setTitle(updateTitle)
+                    builder.setMessage(updateDescription)
+
+                    builder.setPositiveButton(android.R.string.yes) { dialog, which ->
+                        val url = "https://massamare.tk"
+                        val intent = Intent(Intent.ACTION_VIEW)
+                        intent.data = Uri.parse(url)
+                        startActivity(intent)
+                    }
+
+                    builder.setNeutralButton("Cerrar") { dialog, which ->
+
+                    }
+                    builder.show()
+                }
+
+            }
+
+        }
+
+        // endregion
+
+        val button: Button = this.findViewById(R.id.button)
         val config: Button = findViewById(R.id.button3)
 
         val input: EditText = findViewById(R.id.textView)
